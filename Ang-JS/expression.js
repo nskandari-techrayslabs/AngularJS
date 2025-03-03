dbc0d7fcbed66a74986427f6f3eff99f38f0356c
nidhi1513 <nidhikandari1315@gmail.com>
2025-03-03 17:59:45 +0530

    modified

--- Ang-JS/expression.js
+++ Ang-JS/expression.js
@@ -20,8 +20,56 @@
 //     };
 // });
 
-var app = angular.module('myApp',[]);
-        app.controller('myCtrl',function($scope){
-            $scope.name ="";
-            $scope.mail="";
-        });
+// var app = angular.module('myApp',[]);
+//         app.controller('myCtrl',function($scope){
+//             $scope.name ="";
+//             $scope.mail="";
+//         });
+
+// var app = angular.module('myApp',[]);
+// app.controller('myCtrl',function($scope){
+//     $scope.firstName = 'nidhi';
+//     $scope.lastName = 'singh';
+//     $scope.changeName = function(){
+//         $scope.firstName = 'varun';
+//     }
+// });
+
+// var app = angular.module('myApp', []);
+// app.run(function($rootScope) {
+//     $rootScope.color = 'blue';
+// });
+// app.controller('myCtrl', function($scope) {
+//     $scope.color = "red";
+// });
+
+// angular.module('myApp', []).controller('namesCtrl', function($scope) {
+//     $scope.names = [
+//         'Jani',
+//         'Carl',
+//         'Margareth',
+//         'Hege',
+//         'Joe',
+//         'Gustav',
+//         'Birgit',
+//         'Mary',
+//         'Kai'
+//     ];
+// });
+
+angular.module('myApp', []).controller('namesCtrl', function($scope) {
+    $scope.names = [
+      {name:'Jani',country:'Norway'},
+      {name:'Carl',country:'Sweden'},
+      {name:'Margareth',country:'England'},
+      {name:'Hege',country:'Norway'},
+      {name:'Joe',country:'Denmark'},
+      {name:'Gustav',country:'Sweden'},
+      {name:'Birgit',country:'Denmark'},
+      {name:'Mary',country:'England'},
+      {name:'Kai',country:'Norway'}
+    ];
+    $scope.orderByMe = function(x) {
+      $scope.myOrderBy = x;
+    }
+  });
